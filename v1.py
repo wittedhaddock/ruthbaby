e8bd4b26f64fc20d21f72e48223f43e5391dd49c
James William Graham <wittedhaddock@gmail.com>
2015-02-18 18:53:59 -0500

    Data frame dissected into cause and effect: number of runs, strikeouts, and games started versus whether or not player was inducted Response and explanation series created!

--- v1.py
+++ v1.py
@@ -39,3 +39,15 @@ holdout_num = round(len(df.index) * CROSS_VALIDATION_AMOUNT)
 
 testing_indices  = numpy.random.choice(df.index, len(df.index) * 0.2, replace = False)
 training_indices = df.index[~df.index.isin(testing_indices)]
+
+#delineate cause from effect—response from explanation
+response_variables    = df.is_inducted
+explanation_variables = df[["Runs", "Games_started", "Strikeouts_Thrown"]]
+
+#create the response's (whether inducted or not) testing and training series
+response_testing  = response_variables.ix[testing_indices]
+response_training = response_variables.ix[training_indices]
+
+#create explanation's (what is causing the effect: number of runs, games started, and strikouets thrown) training and testing series
+explanation_testing = explanation_variables.ix[testing_indices]
+explanation_training = explanation_variables.ix[training_indices]
