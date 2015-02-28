7d44cdf0207f18533ca9b3f2980671a90621b1e7
James William Graham <wittedhaddock@gmail.com>
2015-02-28 14:15:44 -0500

    test and train indices sampled

--- runs.py
+++ runs.py
@@ -16,9 +16,18 @@ connection = sqlite3.connect("./lahman2013.sqlite")
 
 query = """
 
-SELECT SO as strikeouts, H as hits FROM Batting
+SELECT SO as strikeouts, H as hits, R as runs FROM Batting
 
 """
 df = pandas.read_sql(query, connection)
-
 connection.close()
+
+response_series = df.runs
+explanatory_series = df[["strikeouts", "hits"]]
+
+CV_AMOUNT = 0.2
+
+holdout_number = round(len(df) * CV_AMOUNT,0)
+
+test_indices = numpy.random.choice(df.index, holdout_number, replace = False)
+train_indices = df[~df.index.isin(test_indices)]
