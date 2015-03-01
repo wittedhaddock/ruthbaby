ca2a22aa52c2541c37b7cf620558f3e825722ffa
James William Graham <wittedhaddock@gmail.com>
2015-03-01 18:50:33 -0500

    SQL query, explanatory-response series separated, check to make sure indices in both series mutually map

--- v2.py
+++ v2.py
@@ -4,4 +4,40 @@ Created on Sun Mar  1 18:32:13 2015
 
 @author: james
 """
+from __future__ import division #without this, division acts real whacky
 
+import pandas
+import sqlite3
+import numpy
+import matplotlib.pyplot as plt
+
+connection = sqlite3.connect("./lahman2013.sqlite")
+
+CROSS_VALIDATION_AMOUNT = 0.2
+
+sql_query = """
+SELECT
+    Batting.playerID, Batting.yearID, Batting.R as Runs, Fielding.GS as Games_started, Fielding.playerID, Pitching.playerID, Pitching.SO as Strikeouts_Thrown, HallofFame.inducted as is_inducted, HallofFame.playerID
+FROM 
+    Batting
+LEFT JOIN 
+    Fielding 
+ON 
+    Batting.playerID = Fielding.playerID
+JOIN Pitching ON Fielding.playerID = Pitching.playerID
+JOIN HallofFame ON HallofFame.playerID = Fielding.playerID
+WHERE Batting.yearID < 2000
+GROUP BY Fielding.playerID"""
+
+
+df = pandas.read_sql(sql_query, connection)
+connection.close()
+
+df.count()
+df.dropna(inplace = True)
+explanatory_variables = df[["Strikeouts_Thrown", "Games_started", "Runs"]]
+response_series = df.is_inducted
+
+response_series.index[~response_series.index.isin(explanatory_variables.index)]
+#Assert that above is empty!
+#indices map mutually between both series. Good! 
